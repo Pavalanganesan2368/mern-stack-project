f1cfbcee64a23fee11eaa793a782391a26f7be11
pavalanganesan2368 <pavalanganesan2368@gmail.com>
2025-08-13 10:21:30 +0000

    updated

--- backend/server.js
+++ backend/server.js
@@ -15,17 +15,17 @@ app.use(express.json()); // Allow access to JSON data to req.body
 app.use("/api/product", router);
 
 // Serve frontend in production
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/front-end/dist")));
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/front-end/dist/index.html"));
-    });
-} else {
-    app.get("/", (req, res) => {
-        res.send("API is running...");
-    });
-}
+//     app.get("/*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+//     });
+// } else {
+//     app.get("/", (req, res) => {
+//         res.send("API is running...");
+//     });
+// }
 
 const PORT = process.env.PORT || 5000;
 
